7fcb6b92732361d2b82ab5767e4af92cdc3f78b1
Isthatok74 <terencecyma@gmail.com>
2024-11-29 23:59:06 -0500

    Add ball gravity scale

--- data/states/ballState.go
+++ data/states/ballState.go
@@ -8,12 +8,13 @@ import (
 // represents a game ball, along with all of its state variables
 type BallState struct {
 	BaseState
-	Pos        structures.Vector2 `json:"Pos"`
-	Vel        structures.Vector2 `json:"Vel"`
-	TouchedBy  string             `json:"TouchedBy"`
-	TouchCount int                `json:"TouchCount"`
-	LiveState  string             `json:"LiveState"`
-	ServeState string             `json:"ServeState"`
+	Pos          structures.Vector2 `json:"Pos"`
+	Vel          structures.Vector2 `json:"Vel"`
+	GravityScale float32            `json:"GravityScale"`
+	TouchedBy    string             `json:"TouchedBy"`
+	TouchCount   int                `json:"TouchCount"`
+	LiveState    string             `json:"LiveState"`
+	ServeState   string             `json:"ServeState"`
 }
 
 func (b *BallState) IsAlive() bool {

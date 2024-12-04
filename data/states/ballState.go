d5ba3cdc1b18e483d128e6f1fae8beba5fb4ed17
Isthatok74 <terencecyma@gmail.com>
2024-12-03 23:12:41 -0500

    Add state comments

--- data/states/ballState.go
+++ data/states/ballState.go
@@ -8,15 +8,16 @@ import (
 // represents a game ball, along with all of its state variables
 type BallState struct {
 	BaseState
-	Pos          structures.Vector2 `json:"Pos"`
-	Vel          structures.Vector2 `json:"Vel"`
-	GravityScale float32            `json:"GravityScale"`
-	TouchedBy    string             `json:"TouchedBy"`
-	TouchCount   int                `json:"TouchCount"`
-	LiveState    string             `json:"LiveState"`
-	ServeState   string             `json:"ServeState"`
+	Pos          structures.Vector2 `json:"Pos"`          // the ball's position
+	Vel          structures.Vector2 `json:"Vel"`          // the ball's velocity
+	GravityScale float32            `json:"GravityScale"` // the ball's gravity factor
+	TouchedBy    string             `json:"TouchedBy"`    // the id of the player who last touched the ball
+	TouchCount   int                `json:"TouchCount"`   // the number of touches made on the ball
+	LiveState    string             `json:"LiveState"`    // the live/dead status code of the ball
+	ServeState   string             `json:"ServeState"`   // the service status code of the ball
 }
 
+// returns whether the ball's `LiveState“ indicates that it is alive
 func (b *BallState) IsAlive() bool {
 	return strings.Contains(strings.ToLower(b.LiveState), "alive")
 }
--- data/states/playerState.go
+++ data/states/playerState.go
@@ -7,9 +7,9 @@ import (
 // represents an ingame player, with all of its state variables
 type PlayerState struct {
 	BaseState
-	Pos       st.Vector2 `json:"Pos"`
-	Vel       st.Vector2 `json:"Vel"`
-	FaceRight bool       `json:"FaceRight"`
-	Anim      string     `json:"Anim"`
-	AxisX     float32    `json:"AxisX"`
+	Pos       st.Vector2 `json:"Pos"`       // the player's position
+	Vel       st.Vector2 `json:"Vel"`       // the player's velocity
+	FaceRight bool       `json:"FaceRight"` // the player's facing direction
+	Anim      string     `json:"Anim"`      // the player's primary animation code
+	AxisX     float32    `json:"AxisX"`     // the player's x axis control value
 }

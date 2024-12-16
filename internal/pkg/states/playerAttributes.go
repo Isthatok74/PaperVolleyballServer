76c38094b4c547e1a24e063d5a4f551751cdd8d1
Isthatok74 <terencecyma@gmail.com>
2024-12-16 13:36:43 -0500

    Fix influence level not showing

--- internal/pkg/states/playerAttributes.go
+++ internal/pkg/states/playerAttributes.go
@@ -3,15 +3,15 @@ package states
 // represents the constant variables of a player that is connected to the game
 type PlayerAttributes struct {
 
-	// display name
+	// display information
 	DisplayName string `json:"DisplayName"`
+	Influence   int    `json:"Influence"`
 
 	// player stat levels
 	Strength float32 `json:"Strength"`
 	Speed    float32 `json:"Speed"`
 	Jump     float32 `json:"Jump"`
 	Size     float32 `json:"Size"`
-	Tier     int     `json:"Tier"`
 
 	// visual accessory codes
 	Emblem     string `json:"Emblem"`

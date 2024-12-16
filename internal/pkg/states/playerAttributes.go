d5b0b779b47d5d9b92cc7dd27dd089db7b6c0e9c
Isthatok74 <terencecyma@gmail.com>
2024-12-16 12:42:41 -0500

    Relay accessories

--- internal/pkg/states/playerAttributes.go
+++ internal/pkg/states/playerAttributes.go
@@ -2,10 +2,20 @@ package states
 
 // represents the constant variables of a player that is connected to the game
 type PlayerAttributes struct {
-	DisplayName string  `json:"DisplayName"`
-	Strength    float32 `json:"Strength"`
-	Speed       float32 `json:"Speed"`
-	Jump        float32 `json:"Jump"`
-	Size        float32 `json:"Size"`
-	Tier        int     `json:"Tier"`
+
+	// display name
+	DisplayName string `json:"DisplayName"`
+
+	// player stat levels
+	Strength float32 `json:"Strength"`
+	Speed    float32 `json:"Speed"`
+	Jump     float32 `json:"Jump"`
+	Size     float32 `json:"Size"`
+	Tier     int     `json:"Tier"`
+
+	// visual accessory codes
+	Emblem     string `json:"Emblem"`
+	Hair       string `json:"Hair"`
+	LowerFace  string `json:"LowerFace"`
+	Expression string `json:"Expression"`
 }
